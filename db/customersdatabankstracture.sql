e824ef60ce38ada0dbcb2f7853e5fbcd1d742d9d
Asif Ud Dowla <softengr.asif@gmail.com>
2023-03-26 21:50:38 +0600

    Database structure

---
+++ db/customersdatabankstracture.sql
@@ -0,0 +1,56 @@
+/*
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 10.1.35-MariaDB : Database - customersdatabank
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`customersdatabank` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `customersdatabank`;
+
+/*Table structure for table `customers` */
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `customers` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) DEFAULT NULL,
+  `lastname` varchar(32) DEFAULT NULL,
+  `phone` bigint(11) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `state` varchar(30) DEFAULT NULL,
+  `code` varchar(10) DEFAULT NULL,
+  `ip` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2459481 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `customers_unique` */
+
+DROP TABLE IF EXISTS `customers_unique`;
+
+CREATE TABLE `customers_unique` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) DEFAULT NULL,
+  `lastname` varchar(32) DEFAULT NULL,
+  `phone` bigint(11) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `state` varchar(30) DEFAULT NULL,
+  `code` varchar(10) DEFAULT NULL,
+  `ip` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=262141 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
